d58e30375abb03f12e759076ea43678ca44b1f54
Navin P <navinp0304@gmail.com>
2022-05-12 14:42:48 +0530

    Main coverage

--- gkt-train-merge/src/main/java/com/example/geektrust/Main.java
+++ gkt-train-merge/src/main/java/com/example/geektrust/Main.java
@@ -1,10 +1,7 @@
 package com.example.geektrust;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
-
 public class Main {
+	void Init() {}
 
 	public static void main(String[] args) {
 		Utility util = new Utility();
